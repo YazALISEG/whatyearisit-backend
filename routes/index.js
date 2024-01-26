4fa75ab0b07ea2f5a76c2808f673f1d51899469d
Your name <Your email>
2024-01-26 16:40:25 +0100

    test

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+//router.get('/', function(req, res, next) {
+ // res.render('index', { title: 'Express' });
+//});
 /* GET actual year */
-router.get('/year', function(req, res, newt) {
+router.get('/year', function(req, res) {
   const date = new Date().getFullYear();
 res.json({year: date})
 });
